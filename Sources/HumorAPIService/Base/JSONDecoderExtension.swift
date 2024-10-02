99d4d48771b3136922fede6462b16fe0286060b7
DAO <daoseng33@gmail.com>
2024-10-02 11:06:48 +0800

    major: update humor data model

--- Sources/HumorAPIService/Base/JSONDecoderExtension.swift
+++ Sources/HumorAPIService/Base/JSONDecoderExtension.swift
@@ -11,6 +11,8 @@ extension JSONDecoder {
     static let `default`: JSONDecoder = {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .iso8601
+        
         return decoder
     }()
 }
